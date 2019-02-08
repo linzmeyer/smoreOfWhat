71d4b49dc2ec7d48903be8ab5dcc145097cdffc1
Chase Linzmeyer <47004486+linzmeyer@users.noreply.github.com>
2019-02-08 15:49:53 -0600

    initial commit

---
+++ smoresArray.js
@@ -0,0 +1,46 @@
+/* jshint esversion: 6  */
+let smore = [];
+let smoreStuff = ['thing1', 'thing2', 'thing3'];
+
+// replace things with names.
+smoreStuff.splice(0, smoreStuff.length, 'gramcracker', 'chocolate', 'marshmallow');
+
+// create new vars with relateable names that correlate with smoreStuff elements.
+let gramcracker = smoreStuff[0];
+let chocolate = smoreStuff[1];
+let marshMallow = smoreStuff[2];
+
+// First, you take the gram.
+smore.push( gramcracker );
+
+// You stick the chocolate on the gram.
+smore.push( chocolate );
+
+// Then, you roast the mallow
+marshMallow = roastMallow( marshMallow );
+
+// ...you stick it on the chocolate.
+smore.push( marshMallow );
+
+// Then. you cover it with the other end.
+smore.push ( gramcracker );
+
+// Then, you STUFF!
+
+
+function roastMallow( item ) {
+
+  if ( item === 'marshMallow' ) {
+
+    //roast mallow
+    let flamingMallow = true;
+
+    if ( flamingMallow === true ) {
+      item += ' (roasted)';
+    }
+
+    // blow out flame
+    flamingMallow = false;
+  }
+  return item;
+}
